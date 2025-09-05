cb02091ba8716817cb80edf17ce7076393eb274f
mostafa-azimi <201478367+mostafa-azimi@users.noreply.github.com>
2025-09-05 19:08:35 -0400

    Auto-deploy: Updates at 2025-09-05 19:08:35

--- lib/version.ts
+++ lib/version.ts
@@ -1,4 +1,4 @@
 // Auto-incremented version for deployment tracking
 // This file is updated automatically on each deployment
-export const APP_VERSION = "1.0.90"
-export const BUILD_TIMESTAMP = "2025-09-05T22:55:56.219Z"
+export const APP_VERSION = "1.0.91"
+export const BUILD_TIMESTAMP = "2025-09-05T23:08:35.442Z"
